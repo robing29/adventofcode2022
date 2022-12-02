7dbbf1d308de922c35d2d54bdea909eb0afd5404
Robin Gude <guderobin@gmail.com>
2022-12-02 14:17:39 +0100

    - Dateninput muss noch richtig gesplittet werden

--- aoc2022/aoc2022-day1/Program.cs
+++ aoc2022/aoc2022-day1/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Collections.Generic;
+
+List<string> strings= new List<string>();
+strings = System.IO.File.ReadAllText(@"C:\Users\rgude\source\repos\adventofcode2022\aoc2022\inputs\day1.txt").Split(@"\n\n").ToList();
+
+int maximumCalories = 0;
+int thisElfsCalories = 0;
+
+foreach (string s in strings)
+{
+    
+    if (String.IsNullOrEmpty(s))
+    {
+        if (thisElfsCalories >= maximumCalories)
+        {
+            maximumCalories = thisElfsCalories;
+        }        
+        thisElfsCalories = 0;
+
+    } else
+    {
+        int sInt = int.Parse(s);
+        thisElfsCalories += sInt;
+    }
+    
+
+}
+
+//Part1
+
+
+Console.ReadLine();
+
