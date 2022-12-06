da558d0ca3919f0976d69e8cc7608233deed31ff
Robin Gude <guderobin@gmail.com>
2022-12-06 20:25:22 +0100

    added day 4

---
+++ aoc2022/aoc2022-day4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Data Entry
+
+var a = File.ReadAllLines(@"..\..\..\..\inputs\day4.txt").ToList();
+
+//string.Split(-)
+//If-Statement, beginning of line, end of line included?
+
+//2-4,6-8
+//2 - 3,4 - 5
+//5 - 7,7 - 9
+//2 - 8,3 - 7
+//6 - 6,4 - 6
+//2 - 6,4 - 8
---
+++ aoc2022/aoc2022-day4/cleanup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2022_day4
+{
+    public static class Cleanup
+    {
+        static void CheckRange(int pair1)
+        {
+
+        }
+    }
+}
