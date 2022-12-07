4a76e52ba891239bfbfcf31db1730aae6b8106f1
Robin Gude <guderobin@gmail.com>
2022-12-07 22:38:48 +0100

    day 4 finished, first try on both tasks

--- aoc2022/aoc2022-day4/Program.cs
+++ aoc2022/aoc2022-day4/Program.cs
@@ -1,13 +1,42 @@
 ﻿//Data Entry
+using aoc2022_day4;
 
-var a = File.ReadAllLines(@"..\..\..\..\inputs\day4.txt").ToList();
+List<string> strings= new List<string>();
+strings = File.ReadAllLines(@"..\..\..\..\inputs\day4.txt").ToList();
 
-//string.Split(-)
-//If-Statement, beginning of line, end of line included?
+int counterForContainment = 0;
+int counterForOverlap = 0;
+foreach (string line in strings)
+{
+    var a = line.Split(',');
+    var firsthalf = a[0].Split('-');
+    int.TryParse(firsthalf[0], out int result1);
+    int.TryParse(firsthalf[1], out int result2);
+    int[] firstpart = { result1, result2 };
 
+    var secondhalf = a[1].Split('-');
+    int.TryParse(secondhalf[0], out int result3);
+    int.TryParse(secondhalf[1], out int result4);
+    int[] secondpart = { result3, result4};
+
+    
+
+    if(Cleanup.CheckRange(secondpart, firstpart)){
+        counterForContainment++;
+    }
+    if(Cleanup.CheckOverlap(secondpart, firstpart))
+    {
+        counterForOverlap++;
+    }
+
+}
+
+Console.WriteLine("Task 1: " + counterForContainment);
+Console.WriteLine("Task 2: " + counterForOverlap);
 //2-4,6-8
 //2 - 3,4 - 5
 //5 - 7,7 - 9
 //2 - 8,3 - 7
 //6 - 6,4 - 6
 //2 - 6,4 - 8
+Console.ReadLine();
--- aoc2022/aoc2022-day4/cleanup.cs
+++ aoc2022/aoc2022-day4/cleanup.cs
@@ -4,13 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//2-4,6-8
+//2 - 3,4 - 5
+//5 - 7,7 - 9
+//2 - 8,3 - 7
+//6 - 6,4 - 6
+//2 - 6,4 - 8
+
 namespace aoc2022_day4
 {
     public static class Cleanup
     {
-        static void CheckRange(int pair1)
+        public static bool CheckRange(int[] pair1, int[] pair2)
         {
+            int pair1low = pair1[0];
+            int pair1high = pair1[1];
+            int pair2low = pair2[0];
+            int pair2high = pair2[1];
+            if (pair1low <= pair2low)
+            {
+                if (pair1high >= pair2high)
+                {
+                    return true;
+                }
+            }
+            if (pair2low <= pair1low)
+            {
+                if(pair2high >= pair1high)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
+        public static bool CheckOverlap(int[] pair1, int[] pair2) {
+            int pair1low = pair1[0];
+            int pair1high = pair1[1];
+            int pair2low = pair2[0];
+            int pair2high = pair2[1];
+            //check if one value overlaps
+            if (pair1low == pair2low || pair1low == pair2high || pair1high == pair2high || pair1high == pair2low ) {
+                return true;
+            }
+            //check if pair1 fits into pair2
+            if (pair1low > pair2low && pair1low < pair2high || pair1high < pair2high && pair1high > pair2low)
+            {
+                return true;
+            }
+            //check if pair2 fits into pair1
+            if (pair2low > pair1low && pair2low < pair1high || pair2high < pair1high && pair2high > pair1low)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
