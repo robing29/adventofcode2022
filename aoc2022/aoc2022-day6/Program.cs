9da7fb62b8aa84597237972d053b8bc21807ff86
Robin Gude <guderobin@gmail.com>
2022-12-10 16:33:01 +0100

    added day 5 and 6

---
+++ aoc2022/aoc2022-day6/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualBasic;
+
+///<summary>Data Input</summary>
+///
+File.ReadAllLines(@"..\..\..\..\inputs\day4.txt").ToList();
+
+///<summary>Part 1</summary>
+///
+
+
+///<summary>Part 2</summary>
+///
+
