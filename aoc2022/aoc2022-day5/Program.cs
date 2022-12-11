80fee18ea92b431f538bce1ca227001c6b1adbc2
Robin Gude <guderobin@gmail.com>
2022-12-11 19:57:29 +0100

    - Finished day 5 - Lots of LINQ queries - Help from ChatGPT

--- aoc2022/aoc2022-day5/Program.cs
+++ aoc2022/aoc2022-day5/Program.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.VisualBasic;
+﻿using aoc2022_day5;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 ///<summary>Data Input</summary>
 ///
-File.ReadAllLines(@"..\..\..\..\inputs\day4.txt").ToList();
+List<string> uneditedList = File.ReadAllLines(@"..\..\..\..\inputs\day5.txt").ToList();
+
+List<List<string>> grid = new List<List<string>>();
+var commands = uneditedList.Where(x => x.StartsWith("move"));
+
+int columnCounter = 0;
+
+for (int substringCounter = 0; substringCounter < uneditedList[0].Length; substringCounter += 4)
+{
+    grid.Add(new List<string>());
+    for (int rowCounter = 0; rowCounter < 8; rowCounter++)
+    {
+        grid[columnCounter].Add(uneditedList[rowCounter].Substring(substringCounter, 3).Trim());
+    }
+    columnCounter++;
+}
+grid = grid.Select(x => x.Where(s => !string.IsNullOrEmpty(s)).ToList()).ToList();
+
 
 ///<summary>Part 1</summary>
 ///
 
+//foreach (string s in commands)
+//{
+
+//    Command.MoveStack(grid, s);
+//}
+//Console.WriteLine(Command.EndStack(grid));
 
 ///<summary>Part 2</summary>
 ///
+
+foreach (string s in commands)
+{
+
+    Command.MoveStack9001(grid, s);
+}
+Console.WriteLine(Command.EndStack(grid));
 
---
+++ aoc2022/aoc2022-day5/command.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace aoc2022_day5
+{
+    internal class Command
+    {
+        public static int AmountMoved = 0;
+        private static int FromStackTemp = 0;
+        public static int FromStack => Math.Max(0, FromStackTemp - 1);
+        private static int ToStackTemp = 0;
+        public static int ToStack => Math.Max(0, ToStackTemp - 1);
+
+        public static void FillVariables(string commandline)
+        {
+            Match match = Regex.Match(commandline, @"\d+");
+
+            int.TryParse(match.Value, out AmountMoved);
+            match = match.NextMatch();
+            int.TryParse(match.Value, out FromStackTemp);
+            match = match.NextMatch();
+            int.TryParse(match.Value, out ToStackTemp);
+        }
+        public static void DebugVariables()
+        {
+            Console.WriteLine($"Mein Move {AmountMoved} from {FromStackTemp} to {ToStackTemp}");
+        }
+
+        public static void MoveStack(List<List<string>> grid, string line)
+        {
+            FillVariables(line);
+            var movingStack = grid[FromStack].Take(Math.Min(AmountMoved, grid[FromStack].Count)).ToList();
+            grid[FromStack].RemoveRange(0, Math.Min(AmountMoved, grid[FromStack].Count));
+            foreach (var item in movingStack)
+            {
+                grid[ToStack].Insert(0, item);
+            }
+            DebugVariables();
+            ShowStack(grid);
+        }
+        public static void MoveStack9001(List<List<string>> grid, string line)
+        {
+            FillVariables(line);
+            var movingStack = grid[FromStack].Take(Math.Min(AmountMoved, grid[FromStack].Count)).ToList();
+            grid[FromStack].RemoveRange(0, Math.Min(AmountMoved, grid[FromStack].Count));
+            movingStack.Reverse();
+            foreach (var item in movingStack)
+            {
+                grid[ToStack].Insert(0, item);
+            }
+            DebugVariables();
+            ShowStack(grid);
+        }
+
+        public static void ShowStack(List<List<string>> grid)
+        {
+
+            //Console.Write($"{columnCounter + 1} = {grid[columnCounter].Last()}");
+            Console.WriteLine(string.Join(" ", grid.Select((x, i) => x.Count > 0 ? $"{i + 1} = {x.First()}" : $"{i + 1} = (empty)")));
+        }
+
+        public static string EndStack(List<List<string>> grid)
+        {
+            //return string.Join("",grid.Select((x, i) => x.Count > 0 ? x.Last().Trim(new[] { '[', ']' }) : "empty"));
+            return string.Join(
+                "",
+                grid.Select(
+                    x => x.Count > 0
+                        ? x.First().Trim(new[] { '[', ']' })
+                        : "empty"
+                )
+            );
+        }
+
+    }
+}
