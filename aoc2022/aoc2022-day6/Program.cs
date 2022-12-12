f2506fcdb8c6aaf5842cc52be5a0c9fbead96552
Robin Gude <guderobin@gmail.com>
2022-12-12 10:38:56 +0100

    - finished day 6

--- aoc2022/aoc2022-day6/Program.cs
+++ aoc2022/aoc2022-day6/Program.cs
@@ -1,13 +1,37 @@
 ﻿using Microsoft.VisualBasic;
+using System.Text;
 
 ///<summary>Data Input</summary>
 ///
-File.ReadAllLines(@"..\..\..\..\inputs\day4.txt").ToList();
+string main = File.ReadAllText(@"..\..\..\..\inputs\day6.txt");
 
 ///<summary>Part 1</summary>
 ///
+
+for(int index = 0; index < main.Length; index++)
+{
+    var groupOfFour = main.Substring(index, Math.Min(4, main.Length-index)).Distinct();
+    if (groupOfFour.Count() == 4)
+    {
+        string distinctSequence = string.Join("", groupOfFour);
+        Console.WriteLine(index+4 + " " + distinctSequence);
+        break;
+    }
+}
 
 
 ///<summary>Part 2</summary>
 ///
 
+for (int index = 0; index < main.Length; index++)
+{
+    var groupOfFour = main.Substring(index, Math.Min(14, main.Length - index)).Distinct();
+    if (groupOfFour.Count() == 14)
+    {
+        string distinctSequence = string.Join("", groupOfFour);
+        Console.WriteLine(index + 14 + " " + distinctSequence);
+        break;
+    }
+}
+
+Console.ReadLine();
