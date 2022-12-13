0453d2b18e00cc87158fd6b610b411a592d3fd28
Robin Gude <guderobin@gmail.com>
2022-12-13 18:23:45 +0100

    -day 7

---
+++ aoc2022/aoc2022-day7/Filesystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2022_day7
+{
+    internal class Folder
+    {
+        public string? Name { get; set; }
+        public List<Folder>? Subdirectories { get; set; }
+        public List<File>? Files { get; set; }
+
+        public Folder (List<string> input)
+        {
+            
+        }
+
+    }
+
+    internal class File
+    {
+        public string? Name { get; set; }
+        public int? Size { get; set; }
+
+    }
+}
---
+++ aoc2022/aoc2022-day7/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic;
+
+///<summary>Data Input</summary>
+///
+var input = File.ReadAllLines(@"..\..\..\..\inputs\day7.txt");
+//var groups = input.GroupBy(x => x.StartsWith("$ cd"));
+List<List<string>> filesystem = new List<List<string>>();
+
+
+foreach (var line in input)
+{
+    if (line.StartsWith("$ cd"))
+    {
+        filesystem.Add(new List<string>());
+        filesystem[filesystem.Count-1].Add(line);
+        
+    }
+    else
+    {
+        filesystem[filesystem.Count - 1].Add(line);
+    }
+}
+
+foreach (List<string> test in filesystem)
+{
+    foreach (string str in test)
+    {
+        Console.WriteLine(str);
+    }
+}
+Console.ReadKey();
+
+///<summary>Part 1</summary>
+///
+
+
+///<summary>Part 2</summary>
+///
+
